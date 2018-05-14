7e6422b584b21bb51c53e6d1d7ab1e723041792a
kbaoqq <389565552@qq.com>
2018-05-14 14:04:25 +0800

    更新production

--- app/components/decorateBox.jsx
+++ app/components/decorateBox.jsx
@@ -5,10 +5,17 @@ let User = require('../util/User');
 module.exports = React.createClass({
 	render() {
 		const {propId} = this.props;
+		let name = '';
+		if (User.getInstance().getProp(propId).propType == 1) {
+			name = User.getInstance().getProp(propId).propName;
+		} else {
+			name = `LV${User.getInstance().getProp(propId).propLevel}`;
+		}
+
 		return (
 			<div>
 				<img src={require(`../images/d${propId}l.png`)}/>
-				<span className={'name'}>LV{User.getInstance().getProp(propId).propLevel}</span>
+				<span className={'name'}>{name}</span>
 			</div>
 		)
 	}
